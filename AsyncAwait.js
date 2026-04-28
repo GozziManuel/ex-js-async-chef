7bf49846026d58ed8788d6d14c4b089044d22778
Gozzi Manuel <manuel2006gozzi@gmail.com>
2026-04-28 20:33:12 +0200

    first step

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -1,1 +1,15 @@
+async function handleAsync(url) {
+  const response = await fetch(url);
+  const obj = response.json();
+  return obj;
+}
 
+async function getChefBirthday(id) {
+  const chefBirthday = await handleAsync(`https://dummyjson.com/recipes/${id}`);
+  return chefBirthday;
+}
+
+(async () => {
+  const chef = await getChefBirthday(1);
+  console.log(chef);
+})();
