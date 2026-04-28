76399797585b5cc532851c63b0936e3ca60a2420
Gozzi Manuel <manuel2006gozzi@gmail.com>
2026-04-28 21:22:47 +0200

    finished + bonus 1

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -5,11 +5,40 @@ async function handleAsync(url) {
 }
 
 async function getChefBirthday(id) {
-  const chefBirthday = await handleAsync(`https://dummyjson.com/recipes/${id}`);
-  return chefBirthday;
+  let chefBirthday;
+  try {
+    chefBirthday = await handleAsync(`https://dummyjson.com/recipes/${id}`);
+  } catch (error) {
+    console.error(error);
+  }
+  if (chefBirthday === undefined) {
+    throw new Error("Not found API with that id");
+  }
+
+  let chefUserId;
+  let chefBirthdayDate;
+  try {
+    chefUserId = await handleAsync(
+      `https://dummyjson.com/users/${chefBirthday.userId}`,
+    );
+    chefBirthdayDate = chefUserId.birthDate;
+  } catch (error) {
+    console.error(error);
+  }
+  if (chefBirthdayDate === undefined) {
+    throw new Error("age inexistent");
+  }
+
+  return chefBirthdayDate;
 }
 
 (async () => {
-  const chef = await getChefBirthday(1);
-  console.log(chef);
+  try {
+    const chef = await getChefBirthday(32);
+    console.log("Data di nascita dello chef:", chef);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    console.log("data ricavata!");
+  }
 })();
